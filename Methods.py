40b113dd7b11a39231050a3c0f2964d6c61fd629
agnieszkanowacka <aagnen6021@gmail.com>
2023-06-17 15:29:46 +0200

    Clarified the situation when the key already exists in the dictionary.

--- Methods.py
+++ Methods.py
@@ -18,9 +18,12 @@ def split_to_art(text_string):
         if all_art_list[i] == 'Art.':
             this_art = all_art_list[i+1]
             this_art_split = this_art.split('.')
-            this_art_number = this_art_split[0]
+            this_art_number = this_art_split[0].replace(' ', '')
             this_art_clear_list = this_art_split[1:len(this_art_split)]
             this_art_clear_str = ".".join(this_art_clear_list)
+
+            while f'Art.{this_art_number}' in art_dict:
+                this_art_number = f'{this_art_number}*'
 
             art_dict[f'Art.{this_art_number}'] = f'\n{this_art_clear_str}'
     return art_dict
@@ -38,13 +41,15 @@ def split_to_ustepy(art_dictionary):
         # print(f'{art_key}: {artykul_split}')
 
         # For every ust
-        new_ust_number = " 0"
+        new_ust_number = "0"
         for i in range(0, len(artykul_split)):
             if re.match('((\n)\s*\d{1,4}[a-z]{,3}[.])', artykul_split[i]):
                 new_ust_number = artykul_split[i].replace('.', '').replace(' ', '').replace('\n', '')
                 continue
 
             if artykul_split[i] != " " and artykul_split[i] != "\n" and artykul_split[i] != "":
+                while f'ust.{new_ust_number}' in ust_dict:
+                    new_ust_number = f'{new_ust_number}*'
                 ust_dict[f'ust.{new_ust_number}'] = artykul_split[i]
             # ust_dict[f'ust.{i}'] = artykul_split[i]
 
@@ -69,13 +74,15 @@ def split_to_podpunkty(art_ust_dictionary):
             ust_split = re.split('((\n)\s*\d{1,4}[a-z]{,3}[)])', ust_all)
 
             # For every pdp
-            new_pdp_number = " 0"
+            new_pdp_number = "0"
             for i in range(0, len(ust_split)):
                 if re.match('((\n)\s*\d{1,4}[a-z]{,3}[)])', ust_split[i]):
                     new_pdp_number = ust_split[i].replace(')', '').replace(' ', '').replace('\n', '')
                     continue
 
                 if ust_split[i] != " " and ust_split[i] != "\n" and ust_split[i] != "":
+                    while f'pdp.{new_pdp_number}' in pd_dict:
+                        new_pdp_number = f'{new_pdp_number}*'
                     pd_dict[f'pdp.{new_pdp_number}'] = ust_split[i]
 
             ust_pd_dict[ust_key] = pd_dict
